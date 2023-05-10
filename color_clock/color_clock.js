d085834d0efe7e4c1df46a51f453adc3b135543d
ali barkhordar <alibarkhordar.b@gmail.com>
2023-05-10 14:08:47 -0700

    Add files via upload

--- color_clock/color_clock.js
+++ color_clock/color_clock.js
@@ -17,35 +17,23 @@ function time(){
 
     const changerBySecond = parseInt(second)
 
-    if (changerBySecond < 7){
-        body.style.background = `rgb(${second * 10}, 100, 240)`
-    }
-    else if (changerBySecond >= 6 && changerBySecond < 12){
-        body.style.background = `rgb(255, ${second * 6}, 25)`
-    }
-    else if (changerBySecond >= 12 && changerBySecond < 18){
-        body.style.background = `rgb(230, 255, ${second * 3})`
-    }
-    else if (changerBySecond >= 18 && changerBySecond < 24){
-        body.style.background = `rgb(255, ${second * 2}, 200)`
-    }
-    else if (changerBySecond >= 24 && changerBySecond < 30){
-        body.style.background = `rgb(${second * 2}, 255, 252)`
-    }
-    else if (changerBySecond >= 30 && changerBySecond < 36){
+    if (changerBySecond < 10){
         body.style.background = `rgb(255, ${second}, 188)`
     }
-    else if (changerBySecond >= 36 && changerBySecond < 42){
-        body.style.background = `rgb(${second * 2}, 255, 140)`
+    else if (changerBySecond < 20 && changerBySecond >= 10){
+        body.style.background = `rgb(${second}, 52, 52)`
     }
-    else if (changerBySecond >= 42 && changerBySecond < 48){
-        body.style.background = `rgb(150, 250, ${second})`
+    else if (changerBySecond < 30 && changerBySecond >= 20){
+        body.style.background = `rgb(${second * 10}, 100, 240)`
     }
-    else if (changerBySecond >= 48 && changerBySecond < 54){
-        body.style.background = `rgb(110, ${second}, 0)`
+    else if (changerBySecond < 40 && changerBySecond >= 30){
+        body.style.background = `rgb(230, 255, ${second * 3})`
+    }
+    else if (changerBySecond < 50 && changerBySecond >= 40){
+        body.style.background = `rgb(255, ${second * 6}, 25)`
     }
     else{
-        body.style.background = `rgb(${second}, 52, 52)`
+        body.style.background = `rgb(${second * 2}, 255, 252)`
     }
 
     clock.innerText = `${hour} : ${minute} : ${second}`
